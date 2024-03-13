de863ae2fe39facd9ad9f7da091af355c6434c2a
Prihler <94796499+jenjiru@users.noreply.github.com>
2024-03-13 02:44:04 +0100

    Update main-pc.sh

--- main-pc.sh
+++ main-pc.sh
@@ -5,7 +5,7 @@
 # Updating system
 paru -Syu --noconfirm
 
-install etcher-bin gimp bitwarden spotify lutris multimc-bin jre-openjdk wally-cli qtwebflix-git wacom-settings-git libfido2 surfshark-vpn yubikey-personalization-gui tidal-hifi-bin avidemux-qt youtube-dl upscayl-bin
+install etcher-bin gimp bitwarden spotify lutris multimc-bin jre-openjdk wally-cli qtwebflix-git wacom-settings-git libfido2 yubikey-personalization-gui tidal-hifi-bin avidemux-qt youtube-dl upscayl-bin
 
 # tuta
 install tutanota-desktop-bin
