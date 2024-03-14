069b0c5af2df107820da042c561c12dbc9de0445
jenjen <94796499+jenjiru@users.noreply.github.com>
2024-03-14 17:17:01 +0000

    Update conditions-check.sh

--- conditions-check.sh
+++ conditions-check.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+sudo pacman -S sbctl --noconfirm --needed
+
+# Check if the system is booted with a 64-bit UEFI
 if [ "$(cat /sys/firmware/efi/fw_platform_size  2>/dev/null)" != "64" ]; then
-	echo -e "\033[0;31mError! NOT 64-bit UEFI booted\033[0m" >&2
-	exit  1
+        echo -e "\033[0;31mError! NOT booted in 64-bit UEFI\033[0m" >&2
+        exit  1
+fi
+
+# Check if the system is in Secure Boot setup mode
+if sbctl status | grep -qE "Setup Mode.*Disabled"; then
+    echo -e "\033[0;31mError! Not booted in Secure Boot setup mode\033[0m" >&2
+    exit 1
 fi
