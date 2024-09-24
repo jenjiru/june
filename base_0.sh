a31ab356de3daa1e6522373f70e181dd3feb5685
jenjen <94796499+jenjiru@users.noreply.github.com>
2024-09-24 04:08:00 +0000

    Update base_0.sh

--- base_0.sh
+++ base_0.sh
@@ -8,8 +8,6 @@ sgdisk --zap-all $disk
 # creating partitions
 sgdisk --clear --new=1:0:+512MiB --typecode=1:ef00 --change-name=1:EFI --new=2:0:+4GiB --typecode=2:8200 --change-name=2:cryptswap --new=3:0:0 --typecode=3:8300 --change-name=3:cryptsystem $disk
 
-read
-
 # Encrypted Containers
 cryptsetup luksFormat --type luks2 --align-payload=8192 -s 256 -c aes-xts-plain64 --batch-mode /dev/disk/by-partlabel/cryptsystem <(echo -n "$luks_passwd")
 temp_file=$(mktemp)
@@ -17,14 +15,10 @@ echo -n "$luks_passwd" > "$temp_file"
 trap "rm -f $temp_file" EXIT
 cryptsetup open /dev/disk/by-partlabel/cryptsystem system --key-file "$temp_file"
 
-read
-
 # Swap
 cryptsetup open --type plain --key-file /dev/urandom --batch-mode /dev/disk/by-partlabel/cryptswap swap
 mkswap -L swap /dev/mapper/swap
 swapon -L swap
-
-read
 
 # Btrfs
 mkfs.btrfs --label system /dev/mapper/system
@@ -33,8 +27,6 @@ btrfs subvolume create /mnt/@root
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@snapshots
 umount -R /mnt
-
-read
 
 mount -t btrfs -o defaults,x-mount.mkdir,ssd,noatime,subvol=@root LABEL=system /mnt
 mount -t btrfs -o defaults,x-mount.mkdir,ssd,noatime,subvol=@home LABEL=system /mnt/home
@@ -45,12 +37,8 @@ mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
 mkdir /mnt/efi
 mount LABEL=EFI /mnt/efi
 
-read
-
 # Install Base System
 pacstrap /mnt base linux linux-firmware
 genfstab -L -p /mnt >> /mnt/etc/fstab
 sed -i 's/LABEL=swap/\/dev\/mapper\/swap/g' /mnt/etc/fstab
 echo "swap /dev/disk/by-partlabel/cryptswap /dev/urandom swap,offset=2048,cipher=aes-xts-plain64,size=256" >> /mnt/etc/crypttab
-
-read
